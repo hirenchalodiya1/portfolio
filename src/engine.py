53d1c69d70681de44160ddbdb3ba3936e7adfd66
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2020-02-29 14:41:04 +0530

    Add CAPM in engine

--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,6 @@
 import matplotlib.gridspec as gd
 import matplotlib.pyplot as plt
-from utils import mean_and_cov_matrix
+import numpy as np
 from markovitz import MarkowitzBullet
 from capm import CAPM
 
@@ -9,38 +9,40 @@ class PyfolioEngine:
     def __init__(self, data, expected_mean, risk_free_return, **kwargs):
         # Data
         self.data = data
-        self.ret_matrix, self.cov_matrix = mean_and_cov_matrix(self.data)
-        
+        # self.ret_matrix, self.cov_matrix = mean_and_cov_matrix(self.data)
+
+        # This calculates the mean vector
+        returns_daily = data.pct_change()
+        self.ret_matrix = np.array(returns_daily.mean() * 250)
+
+        # This calculates the covariance matrix
+        cov_daily = returns_daily.cov()
+        self.cov_matrix = np.array(cov_daily * 250)
+
         # Markowitz bullet
-        marko = {}
-        if kwargs.get('marko_mu_min'): marko['mu_min'] = kwargs.get('marko_mu_min')
-        if kwargs.get('marko_mu_max'): marko['mu_max'] = kwargs.get('marko_mu_max')
-        if kwargs.get('marko_mu_gap'): marko['mu_gap'] = kwargs.get('marko_mu_gap')
+        marko = kwargs.get('marko', dict())
         self.marko = MarkowitzBullet(self.ret_matrix, self.cov_matrix, expected_mean, **marko)
 
         # capital assets pricing model
-        cap = {}
-        if kwargs.get('cap_mu_min'): cap['mu_min'] = kwargs.get('cap_mu_min')
-        if kwargs.get('cap_mu_max'): cap['mu_max'] = kwargs.get('cap_mu_max')
-        if kwargs.get('cap_mu_gap'): cap['mu_gap'] = kwargs.get('cap_mu_gap')
+        cap = kwargs.get('cap', dict())
         self.capm = CAPM(self.ret_matrix, self.cov_matrix, expected_mean, risk_free_return, **cap)
-
 
     def plot(self):
         # Figure
         fig = plt.figure()
         fig.canvas.set_window_title('Portfolio optimization')
-        
+
         # Grid
         gs = gd.GridSpec(1, 2, figure=fig)
 
         # Markowitz bullet
         ax1 = fig.add_subplot(gs[0, :1])
         self.marko.plot(ax1)
-        
-        # Capital Assets Pricing Model 
+
+        # Capital Assets Pricing Model
         ax2 = fig.add_subplot(gs[0, 1:])
+        self.marko.plot(ax2, line_only=True)
         self.capm.plot(ax2)
-        
+
         # Show plot
         plt.show()
