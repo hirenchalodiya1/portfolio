49de5741c9c48134691e394cd9a5627d141f501e
Hiren Chalodiya <chalodiya.1@iitj.ac.in>
2020-03-02 00:36:23 +0530

    Calculate beta properly

--- src/betas.py
+++ src/betas.py
@@ -1,10 +1,52 @@
 class Betas:
-    def __init__(self, mean_matrix, market_ret, risk_free_return):
+    def __init__(self, returns_of_stocks, returns_of_market, market_ret, risk_free_return, mean_matrix):
         self.MM = mean_matrix
         self.RR = risk_free_return
         self.MR = market_ret
 
-        self.betas = list()
+        self.betas = dict()
 
-        for mu in self.MM:
-            self.betas.append((mu - self.RR) / (self.MR - self.RR))
+        market_variance = returns_of_market.var()
+        
+        # calculating betas by analysing daily market data
+        for stock, returns in returns_of_stocks.items():
+            beta = returns_of_market.cov(returns)/market_variance
+            self.betas[stock] = beta
+
+        # SML line
+        sml_slope = (self.MR - self.RR) / (1 - 0)
+        self._min_beta = min(0, min(self.betas.values()))
+        self._max_beta = max(1, max(self.betas.values()))
+        self.line_sml = lambda x: self.RR + sml_slope * x
+
+    def plot(self, ax):
+        # x axis
+        ax.axhline(color='#000000')
+
+        # y axis
+        ax.axvline(color='#000000')
+
+        # Add risk free point
+        ax.plot(0, self.RR, label='Risk free point', marker='o')
+
+        # Complete market point
+        ax.plot(1, self.MR, label='Market point', marker='o')
+
+        # SML Line
+        ax.plot([self._min_beta, self._max_beta], [self.line_sml(self._min_beta), self.line_sml(self._max_beta)], label='SML')
+        
+        for mean, beta in zip(self.MM, self.betas.values()):
+            ax.plot(beta, mean, marker='o')
+
+        # Add a title
+        ax.set_title('Security Market Line')
+
+        # Add X and y Label
+        ax.set_xlabel('β axis')
+        ax.set_ylabel('μ axis')
+
+        # Add a grid
+        ax.grid(alpha=.4, linestyle=':')
+
+        # Add a Legend
+        ax.legend(prop={"size": 7})
