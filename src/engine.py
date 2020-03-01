11eebe205af5d9be9754f62d717f48d80cfafaf1
Hiren Chalodiya <chalodiya.1@iitj.ac.in>
2020-03-02 00:36:39 +0530

    Upgrade engine
    - update pretty print
    - update beta calculation

--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,7 @@
 import matplotlib.gridspec as gd
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from markovitz import MarkowitzBullet
 from capm import CAPM
 from betas import Betas
@@ -29,9 +30,18 @@ class PyfolioEngine:
         self.capm = CAPM(self.ret_matrix, self.cov_matrix, expected_mean, risk_free_return, **cap)
 
         # Betas
-        self.betas = Betas(self.ret_matrix, self.capm.ret_risky, risk_free_return)
+        # For market ret and weights matrix for SML, beta calculation
+        # We use derived portfolio value so we need to devied ret_risky and w_risky by sum(w_risky)
+        market_weights = pd.DataFrame(pd.Series(self.capm.w_risky / sum(self.capm.w_risky), index=data.keys()))
+        daily_return_of_market = (returns_daily * market_weights[0]).sum(1)
+        self.betas = Betas(
+                        returns_daily,
+                        daily_return_of_market,
+                        self.capm.ret_risky / sum(self.capm.w_risky),
+                        risk_free_return,
+                        self.ret_matrix)
 
-    def plot(self, show_marko=True, show_capm=True):
+    def plot(self, show_marko=True, show_capm=True, show_beta=True):
         """
         fig1: Markowitz bullet Figure
         fig2: Capital market line
@@ -63,6 +73,20 @@ class PyfolioEngine:
             self.marko.plot(ax2, gp=100, line_only=True)
             self.capm.plot(ax2)
 
+        if show_beta:
+            # Part 3
+            # Figure
+            fig3 = plt.figure()
+            fig3.canvas.set_window_title('Security Market Line')
+
+            # Grid
+            gs3 = gd.GridSpec(1, 1, figure=fig3)
+
+            # plot
+            ax3 = fig3.add_subplot(gs3[:, :])
+            self.betas.plot(ax3)
+
+
         # Show plot
         plt.show()
 
@@ -71,8 +95,8 @@ class PyfolioEngine:
             print('-----------------------------------------------------------------------------------')
             print('Weights for desired portfolio ( w/o risk free ) | Markowitz Theory                 ')
             print('-----------------------------------------------------------------------------------')
-            for i, j in enumerate(zip(self.data, self.marko.w), 1):
-                print('{:2} : {:10s} --> {:.6f}'.format(i, j[0], j[1]))
+            for i, j in enumerate(zip(self.data.keys(), self.marko.w), 1):
+                print('{:2} : {:10s} --> {:10.6f}'.format(i, j[0], j[1]))
             print('-----------------------------------------------------------------------------------')
             print('Observations: ')
             print('1. For given return {:.2f}% minimum risk is {:.2f}%'.format(self.marko.ret*100, self.marko.risk*100))
@@ -82,20 +106,22 @@ class PyfolioEngine:
             print('-----------------------------------------------------------------------------------')
             print('Weights for desired portfolio ( with risk free ) | Capital Assets Pricing Model    ')
             print('-----------------------------------------------------------------------------------')
-            print('{:2} : {:10s} --> {:.6f}'.format(1, 'Risk Free', self.capm.w_risk_free))
-            for i, j in enumerate(zip(self.data, self.capm.w_risky), 2):
-                print('{:2} : {:10s} --> {:.6f}'.format(i, j[0], j[1]))
+            print('{:2} : {:10s} --> {:10.6f}'.format(1, 'Risk Free', self.capm.w_risk_free))
+            for i, j in enumerate(zip(self.data.keys(), self.capm.w_risky), 2):
+                print('{:2} : {:10s} --> {:10.6f}'.format(i, j[0], j[1]))
             print('-----------------------------------------------------------------------------------')
             print('Observations: ')
             print('1. For given return {:.2f}% minimum risk is {:.2f}%'.format(self.capm.ret * 100, self.capm.risk * 100))
-            print('2. In given portfolio for {:.2f}% return obtained by market while, '.format(self.capm.ret_risky))
-            print('   {:.2f}% return obtained by risk free assets'.format(self.capm.ret - self.capm.ret_risky))
+            print('2. In given portfolio for {:.2f}% return obtained by market while, '.format(self.capm.ret_risky * 100))
+            print('   {:.2f}% return obtained by risk free assets'.format((self.capm.ret - self.capm.ret_risky)*100))
             print('3. μ = {:.3f} σ + {:.3f}'.format(self.capm.slope, self.capm.RR))
 
         if show_beta:
             print('-----------------------------------------------------------------------------------')
-            print('List of betas for all the stocks                                                   ')
+            print('     Stock Code    Beta        Return from SML Line   Actual return                     ')
             print('-----------------------------------------------------------------------------------')
-            for i, j in enumerate(zip(self.data, self.betas.betas), 1):
-                print('{:2} : {:10s} --> {:.6f}'.format(i, j[0], j[1]))
+            for i, j in enumerate(zip(self.betas.betas.items(), self.betas.MM), 1):
+                print('{:2} : {:10s} -> {:10.6f} {:15.2f} {:15.2f}'.format(i, j[0][0], j[0][1], self.betas.line_sml(j[0][1]), j[1]))
             print('-----------------------------------------------------------------------------------')
+            print('Observations: ')
+            print('1. Return from SML Line and actual return are same')
